27752e3cd4f63e5713602984273062e63aca1dbc
marcoCoas <marco2206mp@gmail.com>
2026-06-17 11:44:36 -0500

    Opciones del menu

--- src/ejercicio1_CuentaBancaria/CajeroApp.java
+++ src/ejercicio1_CuentaBancaria/CajeroApp.java
@@ -11,6 +11,13 @@ public class CajeroApp {
     public void iniciar(){
         crearCuenta();
         
+        int opcion = 0;
+        while(opcion != 4){
+            mostrarMenu();
+            opcion = lector.leerEntero("Ingrese una opcion");
+            procesarOpcion(opcion);
+        }
+        
         lector.cerrar();
     }
     
@@ -19,8 +26,8 @@ public class CajeroApp {
         double saldo = lector.leerDoubleNoNegativo("Ingrese el saldo inicial");
         
         cuenta = new CuentaBancaria(titular, saldo);
-        System.out.println(cuenta.obtenerInformacion());
-        mostrarMenu();
+        //System.out.println(cuenta.obtenerInformacion());
+        //mostrarMenu();
     }
     
     private void mostrarMenu(){
@@ -29,11 +36,28 @@ public class CajeroApp {
         System.out.println("2. Depositar");
         System.out.println("3. Retirar");
         System.out.println("4. Salir");
-        
-    
     }
     
-    private void procesarOpcion(){
+    private void procesarOpcion(int opcion){
+        switch(opcion){
+            case 1:
+                System.out.println(cuenta.obtenerInformacion());
+                break;
+            case 2:
+                System.out.println("Ingrese el deposito");
+                realizarDeposito();
+                break;
+            case 3:
+                System.out.println("Ingrese el retiro");
+                realizarRetiro();
+                break;
+            case 4:
+                System.out.println("Gracias por usar el sistema");
+                break;
+                
+            default:
+                System.out.println("Opcion INVALIDA");
+        }
     
     }
     
